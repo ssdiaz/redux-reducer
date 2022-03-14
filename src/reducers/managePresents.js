372d4477d1f9c1573b5535eb5a6f185d1c1ff932
Samantha Diaz <samidiaz3@gmail.com>
2022-03-13 23:00:18 -0400

    commit tree f70e95b75ba8f54d1b84b7339f0175d69e6a6164

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,17 @@
 export function managePresents(state, action){
+
+    switch (action.type) {
+        case "INCREASE":
+            // THIS mutates state:
+            // return { numberOfPresents: state.numberOfPresents + 1 };
+
+            //return Object.assign({}, state, {numberOfPresents: state.numberOfPresents + 1})
+
+            //easier to read with spread operator:
+            return Object.assign({...state, numberOfPresents: state.numberOfPresents + 1})
+
+        default:
+            return state;
+    }
+
 }
